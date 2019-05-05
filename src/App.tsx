c9bd0ceb11a530ccb42a0652bf78c8639861025e
jamesliudotcc <james@jamesliu.cc>
2019-05-04 20:29:28 -0700

    json from api prints to screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App: React.FC = () => {
+  // Set offset, this is being used instead of setState.
+  // Implements https://reactjs.org/docs/hooks-overview.html
+  const [offset, setOffset] = useState(1);
+  let [data, setData] = useState();
+
+  const addOffset = () => setOffset(offset + 1);
+  const subtractOffset = () => setOffset(offset > 1 ? offset - 1 : 0);
+
+  // Fetch data
+  // Implements https://www.robinwieruch.de/react-hooks-fetch-data/
+  useEffect(() => {
+    async function fetchMyApi() {
+      const result = await axios(
+        // TODO: I am stumped by why this works but my local API doesn't.
+        `http://localhost:3000/api/${offset}`
+      );
+      setData(result.data);
+    }
+    fetchMyApi();
+  }, [offset]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Offset: {offset}
+      <button onClick={addOffset}>Add Offset</button>
+      <button onClick={subtractOffset}>Subtract Offset</button>
+      <p>{data ? JSON.stringify(data.data) : 'something went wrong'}</p>
     </div>
   );
-}
+};
 
 export default App;
